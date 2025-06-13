dec62b43bffaff0c4b2127e5eb60c4717bb74b26
Hao-Wei <bopytbjr@gmail.com>
2025-06-13 23:29:39 +0100

    Update game.h

--- game.h
+++ game.h
@@ -6,24 +6,10 @@
 #include <string.h>
 #include <time.h>
 
-typedef enum {
-    sixth_sense,
-    camouflage, 
-    healing, 
-    weapon_skill, 
-    mind_shield, 
-    animal_kinship, 
-    num_disciplines
-} Skills;
-
 typedef struct {
     char name[32];
     int endurance;
     int hability;
-    int gold;
-    int skills[8];
-    char * items[20];
-    int nbr_items;
 } Player;
 
 int generate_rnt();
