a90ff001dcca968b0e72ce01d7e2de9928bd0b6b
Lia-0106 <lia.arwenn106@gmail.com>
2025-06-13 15:22:31 +0200

    update

--- export/javascript.js
+++ export/javascript.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const section = document.querySelector("section") ;
-  const h1 = document.createElement("h1") ;
+  const div = document.createElement("div") ;
+  div.className = "content";
 
-  h1.textContent = section.firstChild.textContent.match(/\d+/)[0] ;
-  section.firstChild.remove() ;
-  section.prepend(h1) ;
+  const textNode = section.firstChild ;
+  const number = textNode.textContent.match(/\d+/)[0] ; // Pour récupérer le numéro de section
+
+  const h1 = document.createElement("h1") ;
+  h1.textContent = number ;
+  section.removeChild(textNode) ;
+  div.appendChild(h1) ;
+
+  while (section.firstChild) {
+    div.appendChild(section.firstChild) ;
+  }
+
+  section.appendChild(div) ;
 }) ;
